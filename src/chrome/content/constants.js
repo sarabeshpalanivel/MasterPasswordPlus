812cc7a0aead37a80d48453ef72e550e694b4c9a
V@no <git@vano.org>
2018-08-17 00:21:35 -0400

    v1.30.2 (2018-08-17)
    * support email has been changed

--- src/chrome/content/constants.js
+++ src/chrome/content/constants.js
@@ -1,4 +1,4 @@
-const	EMAIL = "737570706f72742e6d61737465722d70617373776f72642d706c75732e756e69717565383430324076616e6f2e6f7267", //don't want leave email as open text
+const	EMAIL = "737570706f72742e6d61737465722d70617373776f72642d706c75732e756e697175653276676b754076616e6f2e6f7267", //don't want leave email as open text
 			HOMEPAGE = "https://addons.mozilla.org/addon/master-password/",
 			_SUPPORTSITE = "https://github.com/vanowm/MasterPasswordPlus/",
 			SUPPORTSITE = _SUPPORTSITE + "issues",
